bd246242d05affeb86b51458fe0971d2ef10645b
Diego Jiménez Urgell <diegourgell@outlook.com>
2021-08-09 12:02:14 -0500

    Remove fPIC flag

--- travis.sh
+++ travis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "${CI}" == "true" ]]; then
     if [[ "${TRAVIS}" == "true" ]]; then
-        echo "** Overriding src/Makevars and removing C++14 on Travis only"
+        echo "** Define the CXX17 flag for R"
         mkdir -p ~/.R
-        echo 'CXX17 = g++-7 -std=gnu++17 -fPIC' > ~/.R/Makevars
+        echo 'CXX17 = g++-7 -std=gnu++17' > ~/.R/Makevars
     fi
 fi
