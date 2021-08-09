c6e3af2618830617cae8616da50ab63ed2516d8d
Diego Jiménez Urgell <diegourgell@outlook.com>
2021-08-09 11:19:36 -0500

    Change travis.sh to include CXX17

--- travis.sh
+++ travis.sh
@@ -3,5 +3,6 @@ if [[ "${CI}" == "true" ]]; then
     if [[ "${TRAVIS}" == "true" ]]; then
         echo "** Overriding src/Makevars and removing C++14 on Travis only"
         sed -i 's|CXX_STD = CXX17||' src/Makevars
+        echo 'CXX17 = g++-7 -std=gnu++17 -fPIC' > src/Makevars
     fi
 fi
