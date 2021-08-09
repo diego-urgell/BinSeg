1de2fd8ad5866ee18b4dba0d9ee0df0b672c2e6b
Diego Jiménez Urgell <diegourgell@outlook.com>
2021-08-09 11:42:15 -0500

    New appproach with .R/makevars instead of src

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 if [[ "${CI}" == "true" ]]; then
     if [[ "${TRAVIS}" == "true" ]]; then
         echo "** Overriding src/Makevars and removing C++14 on Travis only"
-        sed -i 's|CXX_STD = CXX17||' src/Makevars
-        echo 'CXX17 = g++-7 -std=gnu++17 -fPIC' > src/Makevars
+        mkdir -p ~/.R
+        echo 'CXX17 = g++-7 -std=gnu++17 -fPIC' > ~/.R/Makevars
     fi
 fi
